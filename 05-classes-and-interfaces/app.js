963174b908979a9baf41dc493a09a87b0a29fb3b
Prince <this.is.kumar.prince@gmail.com>
2021-09-25 10:23:16 +0530

    classes done

--- 05-classes-and-interfaces/app.js
+++ 05-classes-and-interfaces/app.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("Hello");
+// interface Person {
+//   name: string;
+//   age: number;
+class Person {
+    name;
+    age = 21;
+    constructor(n) {
+        this.name = n;
+    }
+    greet(phrase) {
+        console.log(phrase + " " + this.name);
+        this.name = "Prince Kumar";
+    }
+}
+new Person("Prince").name = "Prince";
