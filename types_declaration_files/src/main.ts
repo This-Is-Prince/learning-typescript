d9de05890199b615b697976a69cd5ae400646007
Prince <this.is.kumar.prince@gmail.com>
2021-12-09 19:08:42 +0530

    types added

--- types_declaration_files/src/main.ts
+++ types_declaration_files/src/main.ts
@@ -1,8 +1,11 @@
-import './style.css'
+import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+`;
+
+const person: Person = { age: 1, name: "s" };
+const hi: P = { name: "hi" };
---
+++ types_declaration_files/src/types/hi.d.ts
@@ -0,0 +1,3 @@
+interface P {
+  name: string;
+}
