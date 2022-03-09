d8ec1bede7af7914dd1449b253c113d62fc3627f
Prince <this.is.kumar.prince@gmail.com>
2022-03-09 16:33:00 +0530

    typescript vs javascript types completed

---
+++ Understanding_TypeScript/Section_2_TypeScript_Basics_and_Basic_Types/src/12-TypeScript_Types_vs_JavaScript_Types/app.js
@@ -0,0 +1,24 @@
+function first() {
+    function add(n1, n2) {
+        console.log(typeof number1);
+        return n1 + n2;
+    }
+    var number1 = 5;
+    var number2 = 2.8;
+    var result = add(number1, number2);
+    console.log(result);
+}
+function second() {
+    function add(n1, n2) {
+        if (typeof n1 !== "number" || typeof n2 !== "number") {
+            throw new Error("Incorrect input!");
+        }
+        return n1 + n2;
+    }
+    var number1 = '5';
+    var number2 = 2.8;
+    var result = add(number1, number2);
+    console.log(result);
+}
+first();
+second();
