5b9cf77cb869733850488e278769dcf61c8933c6
Prince <this.is.kumar.prince@gmail.com>
2022-03-18 08:25:15 +0530

    working with types completed

---
+++ Understanding_TypeScript/Section_2_TypeScript_Basics_and_Basic_Types/src/14-Working_with_Numbers_Stings&Booleans/app.js
@@ -0,0 +1,15 @@
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var num1 = 5;
+var num2 = 2.8;
+var printResult = true;
+var resultPhrase = "Result is: ";
+var result = add(num1, num2, printResult, resultPhrase);
+console.log(result);
