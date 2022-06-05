63cc6bc21b5513875917a7725a10bd12c1b24b78
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-06-05 07:41:44 +0530

    built in data types completed

---
+++ uday_hiwarale/unit1_introduction/ch_3_built-in_data_types/intro.js
@@ -0,0 +1,12 @@
+// variable declaration
+var person;
+var age = null;
+var car = "Mercedes";
+var canDrive = false;
+console.log("before => (person:".concat(person, "), (age:").concat(age, "), (car:").concat(car, "), (canDrive:").concat(canDrive, ")"));
+// override values
+person = "Ross Geller";
+age = 21;
+car = undefined;
+canDrive = null;
+console.log("before => (person:".concat(person, "), (age:").concat(age, "), (car:").concat(car, "), (canDrive:").concat(canDrive, ")"));
